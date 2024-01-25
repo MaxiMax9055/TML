cb4f7511d09f05ea34d69f0f225f0de8bda3cc84
Gvit0 <90312305+Gvit0@users.noreply.github.com>
2024-01-25 22:17:31 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+repos = "https://github.com/Gvit0/technomuseum"
+verrepos = "https://raw.githubusercontent.com/Gvit0/technomuseum/main/version.txt"
+localrepos = os.path.dirname(os.path.abspath(__file__))
+ipesp32 = "0.0.0.0"
